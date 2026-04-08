261e7bc964fc3cdf753c460c22aa7280a8258138
viktoriia <viktoriia.mykhailova@tetalab.com>
2026-04-08 10:34:50 +0300

    refactor: move constants

--- app/src/main/java/com/hexis/bi/domain/suit/SuitConnectionInfo.kt
+++ app/src/main/java/com/hexis/bi/domain/suit/SuitConnectionInfo.kt
@@ -1,6 +1,8 @@
 package com.hexis.bi.domain.suit
 
+import com.hexis.bi.utils.constants.SuitConstants
+
 data class SuitConnectionInfo(
     val suitId: String,
-    val status: String = "Active",
+    val status: String = SuitConstants.STATUS_ACTIVE,
 )
---
+++ app/src/main/java/com/hexis/bi/utils/constants/SuitConstants.kt
@@ -0,0 +1,5 @@
+package com.hexis.bi.utils.constants
+
+object SuitConstants {
+    const val STATUS_ACTIVE = "Active"
+}
