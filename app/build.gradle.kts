5dcfdc505b2720f8b33c8d6f235bbe0e11e4941b
viktoriia <viktoriia.mykhailova@tetalab.com>
2026-06-05 13:15:01 +0300

    chore: bump v1.0.0(7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         // Terra Android SDK requires minSdk 28 (Samsung Health / Health Connect).
         minSdk = 28
         targetSdk = 37
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
